15eaf7441772a8cee154b84194d82aab7622505a
unknown <brandonellis@gmail.com>
2019-10-23 11:21:50 -0500

    functioning autosuggest in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,103 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import Autosuggest from 'react-autosuggest';
+
+const playerList = require('../playerList.json');
+
+function escapeRegexCharacters(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function getSuggestions(value) {
+  const escapedValue = escapeRegexCharacters(value.trim());
+  
+  if (escapedValue === '') {
+    return [];
+  }
+
+  const regex = new RegExp('^' + escapedValue, 'i');
+
+  return playerList.filter(playerList => regex.test(playerList.fullName));
+}
+
+function getSuggestionValue(suggestion) {
+  return suggestion.fullName;
+}
+
+function renderSuggestion(suggestion) {
+  return (
+    <Link to={'/players/' + suggestion.playerid}><div>{suggestion.fullName}</div></Link>
+  );
+}
 
 
 class Header extends Component {
   constructor() {
     super();
 
-  }
 
+    this.state = {
+      playerList: [],
+      link: '',
+      value: '',
+      suggestions: []
+    };
+  }
+    
+    onChange = (event, { newValue, method }) => {
+      this.setState({
+        value: newValue
+      });
+    };
+      
+    onSuggestionsFetchRequested = ({ value }) => {
+      this.setState({
+        suggestions: getSuggestions(value)
+      });
+    };
+  
+    onSuggestionsClearRequested = () => {
+      this.setState({
+        suggestions: []
+      });
+    };
+    
+    storeInputReference = autosuggest => {
+      if (autosuggest !== null) {
+        this.input = autosuggest.input;
+      }
+    };
  
 
-
   componentDidMount() {
-
+    this.input.focus();
+    
   }
 
 
   render() {
 
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: "Type 'c'",
+      value,
+      onChange: this.onChange
+    };
+
+
     return ( 
       <div className="pb-6 bg-gray-400">
         <div className="container">
+
+        <div className="absolute z-50"><Autosuggest
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps}
+        ref={this.storeInputReference} />
+        </div>
 
                         <li>
                   <Link to='/players/18401'>Acuna</Link>
--- src/components/PlayerBio.js
+++ src/components/PlayerBio.js
@@ -38,16 +38,13 @@ class PlayerBio extends Component {
 
   render() {
 
-     
-    console.log(this.state.playerData);
-
     const { key_mlbam, name_first, name_last, mlb_played_first, mlb_played_last, birth_day, birth_month, birth_year } = this.state.playerData;
 
 
     return ( 
       <div className="bg-gray-900">
         <div className="container p-2 text-right text-white">
-          <div className="absolute z-40 border-solid border-4 border-gray-900 -m-6">
+          <div className="absolute z-10 border-solid border-4 border-gray-900 -m-6">
             <img className="w-32" src={'https://securea.mlb.com/mlb/images/players/head_shot/' + key_mlbam + '.jpg'} onError={(e)=>{e.target.onerror = null; e.target.src="https://via.placeholder.com/213x320.gif"}} alt={name_first + ' ' + name_last} ></img> 
           </div>
           <p className="text-6xl text-center">{name_first} {name_last}</p>
