3c3aba0d32fcb39bb5f628c70cb22de6916aeb29
unknown <brandonellis@gmail.com>
2019-10-25 15:16:44 -0500

    created leaders component

---
+++ src/components/Leaders.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+
+
+class Leaders extends Component {
+
+  constructor(props) {
+    super(props);
+
+
+   
+    this.state = {
+      loading: true,
+      yearid: ""
+    };
+  }
+
+  loadData(yearid) {
+
+
+
+  }
+
+   componentDidMount() {
+
+       if(this.props.match.params.yearid === undefined)
+       {
+        this.setState({yearid: 2019})
+       }
+       else{
+        this.setState({yearid: this.props.match.params.yearid})
+       }
+      this.loadData(this.state.yearid);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.match.params.yearid !== prevProps.yearid) {
+      this.loadData(this.props.match.params.yearid);
+    }
+  }
+
+
+
+
+  render() {
+
+    return ( 
+    
+    <div className="bg-gray-900">
+        <div className="container p-2 text-right text-white">
+                <p className="text-6xl text-center">{ this.props.match.params.yearid ? <span>{this.props.match.params.yearid}</span>: <span>{this.state.yearid}</span> }  Leaders</p>
+    
+        </div>
+    </div>
+  
+    
+    )
+  }
+}
+
+export default Leaders;
+
+
+
+
+
+
+
+
