8d88586a681538aa8e6e62214d96d7646e8dd902
unknown <brandonellis@gmail.com>
2019-10-25 15:16:59 -0500

    took out switch

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Leaders from './Leaders'
 import PlayerProfile from './PlayerProfile'
 
 const Main = () => (
   <main>
-    <Switch>
+    
       <Route exact path='/' component={Leaders}/>
       <Route exact path='/leaders/:yearid' component={Leaders}/>
       <Route path='/players/:id' component={PlayerProfile}/>  
-    </Switch>
+    
   </main>
 )
 
