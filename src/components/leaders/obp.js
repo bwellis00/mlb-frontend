10c8f020a6aa3ec2be60dd13145e1911d147ee27
unknown <brandonellis@gmail.com>
2019-11-03 17:28:10 -0600

    created obp component

---
+++ src/components/leaders/obp.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const OBP = props => (
+  <div className="my-2 px-2 w-1/3 overflow-hidden">
+    <div className="text-2xl pb-3">OBP</div>
+              {props.seasonData.sort((a, b) => b.OBP - a.OBP).filter(data => data.PA > 501).slice(0, 10).map((data, i) =>{
+                      let bgClass = ""
+                      i % 2 ? bgClass = "bg-gray-400" : bgClass= "bg-gray-300"
+                       return (
+                        <Link to={'/players/' + data.playerid}>
+                        <div className={bgClass + " text-black hover:bg-gray-200 cursor-pointer flex"}>
+                          <div className="w-2/3">{data.Name}</div>
+                          <div className="w-1/3">{data.OBP.toFixed(3).slice(1,5)}</div>
+                        </div>
+                        </Link>
+
+              
+                       )
+                      
+                     }
+                    )
+                  }
+  </div>
+)
+
+export default OBP
